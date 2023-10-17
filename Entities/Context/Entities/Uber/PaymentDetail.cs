f84af7203e4bc7bda1b98de2383845c8d19b07a6
Ricardo Santiago Hernandez <ricardo.santiago@vemo.com.mx>
2023-10-16 22:18:32 -0600

    Adjust namespace

--- Entities/Context/Entities/Uber/PaymentDetail.cs
+++ Entities/Context/Entities/Uber/PaymentDetail.cs
@@ -1,6 +1,8 @@
 ﻿using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
+namespace Entities.Context.Entities.Uber;
+
 public class PaymentDetail
 {
     [Key]

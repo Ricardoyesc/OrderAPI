65540d54dc178a5ec35536c17e4e1a07f2f76587
Ricardo Santiago Hernandez <ricardo.santiago@vemo.com.mx>
2023-10-19 20:34:09 -0600

    Fix Didi header

--- OrderAPI/Policies/Handlers/DiDiHeaderSignatureHandler.cs
+++ OrderAPI/Policies/Handlers/DiDiHeaderSignatureHandler.cs
@@ -16,38 +16,42 @@ namespace OrderAPI.Policies.Handlers
                 return;
             }
 
+            request.Request.EnableBuffering();
+
             // Extract the header sign value
             string headerSign = request.Request.Headers["didi-header-sign"];
 
-            // Read the request body
-            request.Request.EnableBuffering();
-            using var reader = new StreamReader(request.Request.Body);
-            string requestBody = await reader.ReadToEndAsync();
-            reader.Close();
-
-            // Calculate the expected signature
-            string expectedSignature;
-
-            // In this example, we'll use MD5 for signature calculation
-            using (var md5 = MD5.Create())
+            // Read the request body into a MemoryStream
+            using (var memoryStream = new MemoryStream())
             {
-                // Concatenate the request body and app secret
-                string signStr = requestBody + requirement.AppSecret;
-                byte[] hashBytes = md5.ComputeHash(Encoding.UTF8.GetBytes(signStr));
-                expectedSignature = BitConverter.ToString(hashBytes).Replace("-", "").ToLower();
-            }
+                await request.Request.Body.CopyToAsync(memoryStream);
+                memoryStream.Seek(0, SeekOrigin.Begin);
 
-            // Compare the header sign with the expected signature
-            if (string.Equals(headerSign, expectedSignature, StringComparison.OrdinalIgnoreCase))
-            {
-                context.Succeed(requirement);
-            }
-            else
-            {
-                context.Fail();
-            }
+                // Convert the MemoryStream content to a string
+                string requestBody = Encoding.UTF8.GetString(memoryStream.ToArray());
 
-            // Reset the request body position
+                // Calculate the expected signature
+                string expectedSignature;
+
+                // In this example, we'll use MD5 for signature calculation
+                using (var md5 = MD5.Create())
+                {
+                    // Concatenate the request body and app secret
+                    string signStr = requestBody + requirement.AppSecret;
+                    byte[] hashBytes = md5.ComputeHash(Encoding.UTF8.GetBytes(signStr));
+                    expectedSignature = BitConverter.ToString(hashBytes).Replace("-", "").ToLower();
+                }
+
+                // Compare the header sign with the expected signature
+                if (string.Equals(headerSign, expectedSignature, StringComparison.OrdinalIgnoreCase))
+                {
+                    context.Succeed(requirement);
+                }
+                else
+                {
+                    context.Fail();
+                }
+            }
             request.Request.Body.Position = 0;
         }
     }
